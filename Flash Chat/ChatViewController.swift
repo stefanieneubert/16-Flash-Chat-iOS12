41cd8da04f6e92c04b5edf02b88baf5e14da6bf9
Stefanie Neubert <stefanie.neubert@jls.ch>
2019-08-09 16:28:56 +0200

    Add logout

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 class ChatViewController: UIViewController {
@@ -106,7 +107,12 @@ class ChatViewController: UIViewController {
     
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
-        //TODO: Log out the user and send them back to WelcomeViewController
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch {
+            print("Error signing out")
+        }
         
         
     }
